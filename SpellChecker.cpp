93d67773a888c288310f30e1dde41432a135c30e
ronlee9251979 <ronald@westwinelements.com>
2026-08-13 13:20:26 -0500

    Ronnies part

--- SpellChecker.cpp
+++ SpellChecker.cpp
@@ -7,7 +7,6 @@
 ** Date: 08/16/2026
 ** -------------------------------------------------------------------------*/
 
-#include <iostream>
 #include <fstream>
 
 using namespace std;
@@ -16,6 +15,50 @@ int main()
 {
     
     // R. Lee: User input for message and keyword
+int main()
+{
+    string wordListFileName;
+    string textFileName;
+
+    ifstream wordListFile;
+    ifstream textFile;
+
+    cout << "Spell Checker Program" << endl;
+    cout << "---------------------" << endl;
+
+    // Ask the user for the correctly spelled word-list file.
+    cout << "Enter the name of the word-list file: ";
+    getline(cin, wordListFileName);
+
+    wordListFile.open(wordListFileName);
+
+    // Verify that the word-list file opened successfully.
+    if (!wordListFile)
+    {
+        cerr << "Error: Unable to open " << wordListFileName << "." << endl;
+        return 1;
+    }
+
+    // Ask the user for the text file to check.
+    cout << "Enter the name of the text file to check: ";
+    getline(cin, textFileName);
+
+    textFile.open(textFileName);
+
+    // Verify that the text file opened successfully.
+    if (!textFile)
+    {
+        cerr << "Error: Unable to open " << textFileName << "." << endl;
+        return 1;
+    }
+
+    cout << "\nBoth files opened successfully." << endl;
+    cout << "Checking the spelling in " << textFileName << "..." << endl;
+
+    // The word-list loading and spell-checking code goes here.
+
+    wordListFile.close();
+    textFile.close();
 
     //A. Lariviere: Keyword validation and Cipher implementation
     
